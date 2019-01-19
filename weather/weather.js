4a791af299efad15ea4924b5363684fc21e08686
Mohamed Abdel Aziz <aziz@192.168.1.6>
2019-01-19 22:13:32 +0200

    add geo to get longitude and latitude from url add wether api to get it correct

---
+++ weather/weather.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+var getWeather = (lat, lng, callback) => {
+  request({
+    url: `https://api.forecast.io/forecast/3267b283ec2071ff42c0bf3fdc334a80/${lat},${lng}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      callback('Unable to connect to Forecast.io server.');
+    } else if (response.statusCode === 400) {
+      callback('Unable to fetch weather.');
+    } else if (response.statusCode === 200) {
+      callback(undefined, {
+        temperature: body.currently.temperature,
+        apparentTemperature: body.currently.apparentTemperature
+      });
+    }
+  });
+};
+
+module.exports.getWeather = getWeather;
